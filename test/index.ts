144b35e1fc9bf1c0bc491f2fcb1cbad0cc9c0fb8
Dominic Carretto <trimoxal@gmail.com>
2018-10-06 20:38:01 -0400

    ci: Improve unit test speeds
    Closes #1406

--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,15 @@
 import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/zone.min.js';
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare const __karma__: any;
 declare const require: any;
 
-// Prevent Karma from running prematurely.
-__karma__.loaded = function() { };
-
+// First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
@@ -26,4 +17,3 @@ getTestBed().initTestEnvironment(
 
 const context = require.context('./', true, /\.test\.ts$/);
 context.keys().map(context);
-__karma__.start();
