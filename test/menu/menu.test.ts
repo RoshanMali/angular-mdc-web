cd6953c33c4bf03606fbf5a61a9218c8825f5f36
Dominic Carretto <trimoxal@gmail.com>
2018-10-07 11:31:36 -0400

    test(menu): Improve menu test coverage

--- test/menu/menu.test.ts
+++ test/menu/menu.test.ts
@@ -2,7 +2,14 @@ import { Component, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { MdcMenu, MdcMenuModule, MdcListModule } from '@angular-mdc/web';
+import {
+  MdcMenu,
+  MdcMenuModule,
+  MdcListModule,
+  MdcListItem,
+  MdcMenuSelectionGroup,
+  MdcMenuSelectionGroupIcon
+} from '@angular-mdc/web';
 
 describe('MdcMenu', () => {
   let fixture: ComponentFixture<any>;
@@ -50,6 +57,12 @@ describe('MdcMenu', () => {
       expect(testInstance.quickOpen).toBe(true);
     });
 
+    it('#menu should set fixed', () => {
+      testComponent.fixed = false;
+      fixture.detectChanges();
+      expect(testInstance.fixed).toBe(false);
+    });
+
     it('#menu should set anchor corner', () => {
       testComponent.anchorCorner = 'topEnd';
       fixture.detectChanges();
@@ -62,10 +75,29 @@ describe('MdcMenu', () => {
       testComponent.anchorCorner = 'bottomStart';
       fixture.detectChanges();
       expect(testInstance.anchorCorner).toBe('bottomStart');
+
+      expect(testInstance.absolutePosition).toBeDefined();
+      expect(testInstance.anchorMargin).toBeDefined();
+      expect(testInstance.anchor).toBeDefined();
     });
 
     it('#should handle click event', () => {
       testDebugElement.nativeElement.click();
+      fixture.detectChanges();
+    });
+
+    it('#should handle body click event', () => {
+      document.body.click();
+      fixture.detectChanges();
+      expect(testInstance.open).toBe(false);
+    });
+
+    it('#should handle list item click', () => {
+      const listItemDebugElement = fixture.debugElement.query(By.directive(MdcListItem));
+      const listItemInstance = listItemDebugElement.injector.get<MdcListItem>(MdcListItem);
+
+      listItemInstance.getListItemElement().click();
+      fixture.detectChanges();
     });
   });
 });
@@ -73,12 +105,17 @@ describe('MdcMenu', () => {
 @Component({
   template: `
     <div mdcMenuSurfaceAnchor #testanchor>
-      <mdc-menu [open]="open" [anchorCorner]="anchorCorner" (select)="handleSelect($event)" [anchor]="testanchor" [quickOpen]="quickOpen"
-        [absolutePosition]="{ x: 5, y: 2}" fixed="fixed" [anchorMargin]="{top: 0, right: 0, bottom: 0, left: 0}">
-        <mdc-list>
-          <mdc-list-item>Item 1</mdc-list-item>
-          <mdc-list-item>Item 2</mdc-list-item>
-        </mdc-list>
+      <mdc-menu [open]="open" [anchorCorner]="anchorCorner" (select)="handleSelect($event)"
+       [anchor]="testanchor" [quickOpen]="quickOpen" [absolutePosition]="{ x: 5, y: 2}"
+       [fixed]="fixed" [anchorMargin]="{top: 0, right: 0, bottom: 0, left: 0}">
+         <mdc-menu-selection-group>
+           <div mdcMenuSelectionGroupIcon>
+            <mdc-list>
+              <mdc-list-item>Item 1</mdc-list-item>
+              <mdc-list-item>Item 2</mdc-list-item>
+            </mdc-list>
+          </div>
+         </mdc-menu-selection-group>
       </mdc-menu>
     </div>
   `,
@@ -87,7 +124,7 @@ class MenuTest {
   open: boolean;
   anchorCorner: string = 'topStart';
   quickOpen: boolean;
-  fixed: boolean;
+  fixed: boolean = true;
 
   handleSelect(event) { }
 }
