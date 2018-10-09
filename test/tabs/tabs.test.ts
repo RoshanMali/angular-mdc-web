9ea171fa2ccfebb20e4e35605e14233cf5aade92
Dominic Carretto <trimoxal@gmail.com>
2018-10-08 20:16:10 -0400

    test(tab): Improve Tab test coverage

--- test/tabs/tabs.test.ts
+++ test/tabs/tabs.test.ts
@@ -1,8 +1,11 @@
 import { Component, DebugElement, ViewChild } from '@angular/core';
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
+import { dispatchFakeEvent, dispatchKeyboardEvent } from '../testing/dispatch-events';
+
 import {
+  LEFT_ARROW,
   MdcTabBarModule,
   MdcIconModule,
   MdcTabBar,
@@ -42,46 +45,76 @@ describe('MDC Tabs', () => {
       expect(testDebugElement.nativeElement.classList).toContain('mdc-tab-bar');
     });
 
+    it('#should make second tab active', fakeAsync(() => {
+      dispatchFakeEvent(testInstance.tabs.toArray()[1].elementRef.nativeElement, 'click');
+      fixture.detectChanges();
+      flush();
+    }));
+
+    it('#should make third tab active', fakeAsync(() => {
+      dispatchFakeEvent(testInstance.tabs.toArray()[1].elementRef.nativeElement, 'click');
+      fixture.detectChanges();
+      flush();
+
+      dispatchFakeEvent(testInstance.tabs.toArray()[2].elementRef.nativeElement, 'click');
+      fixture.detectChanges();
+      flush();
+    }));
+
+    it('#should handle keydown', fakeAsync(() => {
+      dispatchKeyboardEvent(testInstance.tabs.toArray()[1].elementRef.nativeElement, 'keydown', LEFT_ARROW);
+      fixture.detectChanges();
+      flush();
+    }));
+
     it('#should make first tab active', fakeAsync(() => {
       testInstance.activateTab(0);
       fixture.detectChanges();
-      tick(1000);
+      flush();
 
       expect(testInstance.tabs.toArray()[0].elementRef.nativeElement.classList.contains('mdc-tab--active')).toBe(true);
+    }));
+
+    it('#should make first tab disabled', fakeAsync(() => {
+      testInstance.disableTab(0, true);
+      fixture.detectChanges();
+      flush();
+
+      expect(testInstance.tabs.toArray()[0].elementRef.nativeElement.classList.contains('ng-mdc-tab--disabled')).toBe(true);
     }));
 
     it('#should turn on stacked for tabs', fakeAsync(() => {
       testComponent.stacked = true;
       fixture.detectChanges();
-      tick(1000);
+      flush();
       expect(testInstance.tabs.toArray()[0].stacked).toBe(true);
     }));
 
     it('#should turn on fixed for tabs', fakeAsync(() => {
       testComponent.fixed = true;
       fixture.detectChanges();
-      tick(1000);
+      flush();
       expect(testInstance.tabs.toArray()[0].fixed).toBe(true);
     }));
 
     it('#should turn on fade indicator', fakeAsync(() => {
       testComponent.fade = true;
       fixture.detectChanges();
-      tick(1000);
+      flush();
       expect(testInstance.tabs.toArray()[0].tabIndicator.fade).toBe(true);
     }));
 
     it('#should turn on automatic activation behavior', fakeAsync(() => {
       testComponent.useAutomaticActivation = true;
       fixture.detectChanges();
-      tick(1000);
+      flush();
       expect(testInstance.useAutomaticActivation).toBe(true);
     }));
 
     it('#should set active tab', fakeAsync(() => {
       testComponent.activeTabIndex = 1;
       fixture.detectChanges();
-      tick(1000);
+      flush();
       expect(testInstance.getActiveTabIndex()).toBe(1);
       expect(testInstance.getActiveTab()).toBeDefined();
     }));
@@ -89,7 +122,7 @@ describe('MDC Tabs', () => {
     it('#should set alignment to end', fakeAsync(() => {
       testComponent.align = 'end';
       fixture.detectChanges();
-      tick(1000);
+      flush();
       expect(testInstance.tabScroller.align).toBe('end');
       expect(testInstance.tabScroller.incrementScroll(20));
       expect(testInstance.tabScroller.getScrollContentWidth()).toBeDefined();
